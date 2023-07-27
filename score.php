7ddeed65f31ffdee1752e598a2cc9b028f9a192d
ToshinoriKasuya <toshinori2589@gmail.com>
2023-07-27 13:07:07 +0900

    message

---
+++ score.php
@@ -0,0 +1,19 @@
+<!DOCTYpe html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題005</title>
+
+</head>
+
+<body>
+    <?php
+       $score1〜score10=80+60+55+40+100+25+80+95+30+60;
+echo $score1〜score10 /10;
+
+
+    ?>
+</body>
+</html>
+
